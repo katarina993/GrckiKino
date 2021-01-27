7f77c053b0f74257e45b7d86ae169eb322ff62f6
Katarina Tomic <katarina93pa@hotmail.com>
2021-01-27 12:24:53 +0100

    Setup project

---
+++ GrckiKino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrckiKino
+//
+//  Created by Katarina Tomic on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
